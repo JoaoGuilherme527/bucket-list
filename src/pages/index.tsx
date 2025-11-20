7dcf4ba7456a6783ceea1e559fcb6774d2a365f7
JoaoGuilherme527 <168598901+JoaoGuilherme527@users.noreply.github.com>
2025-11-20 01:17:39 -0300

    FIX css problem

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -83,7 +83,6 @@ export default function Home() {
             <Head>
                 <title>Nosso Bucket List & Guia</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <script src="https://cdn.tailwindcss.com"></script>
                 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
             </Head>
 
